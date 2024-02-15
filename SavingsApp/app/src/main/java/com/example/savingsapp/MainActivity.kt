ee8d8f63a2aa8d80c300e622789e3ddec4bbde81
Idan Revivo <idan.revivo@gmail.com>
2024-02-15 21:50:04 +0200

    Learning by playing around

--- SavingsApp/app/src/main/java/com/example/savingsapp/MainActivity.kt
+++ SavingsApp/app/src/main/java/com/example/savingsapp/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.savingsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.savingsapp.ui.theme.SavingsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,10 +34,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column (modifier = Modifier
+        .background(color = Color.Black)
+        .fillMaxSize()){
+        Text(
+            text = "Hello $name!",
+            fontSize = 30.sp,
+            color = Color.Cyan,
+            modifier = modifier
+
+
+
+        )
+        Text(
+            text = "I enjoy this too much",
+            fontSize = 30.sp,
+            color = Color.Red
+
+        )
+    }
+
 }
 
 @Preview
